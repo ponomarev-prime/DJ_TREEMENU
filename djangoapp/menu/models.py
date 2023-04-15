3e615d31702a8341a095f58b0ffb66cc714b9d6b
Alex <ax.dev@yahoo.com>
2023-04-15 19:54:01 +0300

    Fixed

--- djangoapp/menu/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=50)
-    url = models.CharField(max_length=200)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
